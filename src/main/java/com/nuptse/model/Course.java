dab3d8481464e652e922b9ffc6c21baefa079ba4
prashant2071 <pbt9840@gmail.com>
2019-06-20 06:19:22 +0545

    First Commit

--- src/main/java/com/nuptse/model/Course.java
+++ src/main/java/com/nuptse/model/Course.java
@@ -24,58 +24,67 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Entity
 @Table(name = "course")
 public class Course {
-	    @Id
-	    @GeneratedValue(strategy = GenerationType.AUTO)
-	    @Column(name = "id")
-	    private int courseId;
-	    @Column(name = "name")
-	    @NotEmpty(message = "*Please provide your course name")
-	    private String courseName;
-	    @Column(name = "description")
-	    @NotEmpty(message = "*Please provide course description")
-	    private String description;
-	    @Column(name = "study_hours")
-	    @NotEmpty(message = "*Please provide study hours")
-	    @Size(min=1, max=30) 
-	    private String studyHours;
-	    @Column(name = "start_date")
-	    @NotEmpty(message = "*Please provide course reference book name")
-	   // @DateTimeFormat(pattern = "yyyy-mm-dd");
-	    private String startDate;
-	    
-		public int getCourseId() {
-			return courseId;
-		}
-		public void setCourseId(int courseId) {
-			this.courseId = courseId;
-		}
-		public String getCourseName() {
-			return courseName;
-		}
-		public void setCourseName(String courseName) {
-			this.courseName = courseName;
-		}
-		public String getDescription() {
-			return description;
-		}
-		public void setDescription(String description) {
-			this.description = description;
-		}
-		public String getStudyHours() {
-			return studyHours;
-		}
-		public void setStudyHours(String studyHours) {
-			this.studyHours = studyHours;
-		}
-		public String getStartDate() {
-			return startDate;
-		}
-		public void setStartDate(String startDate) {
-			this.startDate = startDate;
-		}
-		
-		public String toString(){
-			return this.courseName;
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int courseId;
+	@Column(name = "name")
+	@NotEmpty(message = "*Please provide your course name")
+	private String courseName;
+	@Column(name = "description")
+	@NotEmpty(message = "*Please provide course description")
+	private String description;
+	@Column(name = "study_hours")
+	@NotEmpty(message = "*Please provide study hours")
+	@Size(min = 1, max = 30)
+	private String studyHours;
+	@Column(name = "start_date")
+	@NotEmpty(message = "*Please provide course reference book name")
+	// @DateTimeFormat(pattern = "yyyy-mm-dd");
+	private String startDate;
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStudyHours() {
+		return studyHours;
+	}
+
+	public void setStudyHours(String studyHours) {
+		this.studyHours = studyHours;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	// public String toString(){
+	// return this.courseName;
+	// }
 
 }
